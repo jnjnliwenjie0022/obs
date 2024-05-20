097a0ca8db22f65729335f6b3ad7624d8abfa357
jnjnliwenjie0022 <jnjn0022@gmail.com>
2024-05-20 23:50:36 +0800

    vault backup: 2024-05-20 23:50:36

--- dv/cast/main.cpp
+++ dv/cast/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class {
-public: virtual ~base() {
+class root {
+public:
+    virtual ~root () { }
+    virtual void print () {
+        cout << "is root" << endl;
+    }
+};
+
+class stem : public root {
+public:
+    virtual print () {
+        cout << "is stem" << endl;
+    }
+}
+
+class leaf : public stem {
+public:
+    virtual print () {
+        cout << "is leaf" << endl;
     }
 }
 
 int main() {
+    root* root_p = new root;
+    leaf* leaf_p = new leaf;
+
+    stem* stem_root_p = dynamic_case<stem*>(root);
+    stem* stem_left_p = dynamic_case<stem*>(stem);
 }
