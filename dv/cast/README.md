6bccbee8d048c495598ecdef9e078161226a90d1
jnjnliwenjie0022 <jnjn0022@gmail.com>
2024-05-21 00:01:36 +0800

    vault backup: 2024-05-21 00:01:36

---
+++ dv/cast/README.md
@@ -0,0 +1,5 @@
+pointer display 0 means fatal.
+stem_root_p:0
+stem_leaf_p:0x55d38e63fed0
+stem_root_stem_p:0x55d38e63fef0
+stem_stem_leaf_p:0x55d38e63fed0
--- dv/cast/main.cpp
+++ dv/cast/main.cpp
@@ -11,22 +11,33 @@ public:
 
 class stem : public root {
 public:
-    virtual print () {
+    virtual void print () {
         cout << "is stem" << endl;
     }
-}
+};
 
 class leaf : public stem {
 public:
-    virtual print () {
+    virtual void print () {
         cout << "is leaf" << endl;
     }
-}
+};
 
 int main() {
     root* root_p = new root;
     leaf* leaf_p = new leaf;
+    root* root_stem_p = new stem;
 
-    stem* stem_root_p = dynamic_case<stem*>(root);
-    stem* stem_left_p = dynamic_case<stem*>(stem);
+    stem* stem_root_p = dynamic_cast<stem*>(root_p);
+    stem* stem_leaf_p = dynamic_cast<stem*>(leaf_p);
+    stem* stem_root_stem_p = dynamic_cast<stem*>(root_stem_p);
+    stem* stem_stem_leaf_p = dynamic_cast<stem*>(stem_leaf_p);
+
+    cout << "pointer display 0 means fatal." << endl;
+    cout << "stem_root_p:" << stem_root_p << endl;
+    cout << "stem_leaf_p:" << stem_leaf_p << endl;
+    cout << "stem_root_stem_p:" << stem_root_stem_p << endl;
+    cout << "stem_stem_leaf_p:" << stem_stem_leaf_p << endl;
+
+    return 0;
 }
