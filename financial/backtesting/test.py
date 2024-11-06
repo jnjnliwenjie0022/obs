03a2ae10c6794038579ec0362b144459a1f66d9f
jnjnliwenjie0022 <jnjn0022@gmail.com>
2024-11-06 13:38:52 +0800

    vault backup: 2024-11-06 13:38:51

--- financial/backtesting/test.py
+++ financial/backtesting/test.py
@@ -1,18 +1,2 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import requests
+https://histock.tw/stock/brokertrace.aspx?bno=1590&no=1301
 
-driver = webdriver.Chrome('chromedriver')
-driver.implicitly_wait(10)
-driver.get('https://histock.tw/stock/branch.aspx?no=1301')
-print(driver.title)
-html = driver.page_source
-print(html)
-
-soup = BeautifulSoup(driver.page_source, 'lxml')
-print(soup.prettify())
-
-with open('index.html', 'w', encoding='utf-8',) as file:
-    file.write(soup.prettify())
-
-driver.quit()
