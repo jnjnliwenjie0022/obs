23ec2e8eae5f50bcaf4869004ee78acb7c12b0ac
jnjnliwenjie0022 <jnjn0022@gmail.com>
2024-11-09 01:45:45 +0800

    vault backup: 2024-11-09 01:45:44

--- financial/backtesting/scrapyproject/scrapyproject/spiders/bank.py
+++ financial/backtesting/scrapyproject/scrapyproject/spiders/bank.py
@@ -4,7 +4,10 @@ import scrapy
 class BankSpider(scrapy.Spider):
     name = "bank"
     allowed_domains = ["concords.moneydj.com"]
+    #start_urls = ["https://histock.tw/stock/mainprofit.aspx?no=1301&day=7"]
+
     start_urls = ["https://concords.moneydj.com/z/zc/zco/zco_1101.djhtm"]
+
 
     def parse(self, response):
         pass
